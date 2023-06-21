0519ebce831522fa01b978470aa0f3b72b4bf478
TaekyungAn <atk0426@naver.com>
2023-06-21 22:15:57 +0900

    23.06.21

--- section7/section7-11.js
+++ section7/section7-11.js
@@ -1,6 +1,33 @@
+// 23.06.21 강의 (결정알고리즘/이분검색)
+// 와 어렵다
+
+// 아래 함수를 짜는게 와우임
 function count(songs, capacity) {
   let cnt = 1,
     sum = 0;
+  for (let x of songs) {
+    if (sum + x > capacity) {
+      cnt++;
+      sum = x;
+    } else sum += x;
+  }
+  return cnt;
+}
+// 이분검색
+function solution(m, songs) {
+  let answer;
+  let lt = Math.max(...songs);
+  let rt = songs.reduce((a, b) => a + b, 0);
+  // 아래 이분검색 공식은 그냥 외우기
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (count(songs, mid) <= m) {
+      answer = mid;
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
   return answer;
 }
 
